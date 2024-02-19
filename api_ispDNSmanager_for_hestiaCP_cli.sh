1dff82fcb77eff46353607643efbb00e88a6eb4c
zDimaBY <zdimaby@gmail.com>
2024-02-20 01:28:07 +0200

    v0.4

--- api_ispDNSmanager_for_hestiaCP_cli.sh
+++ api_ispDNSmanager_for_hestiaCP_cli.sh
@@ -6,10 +6,10 @@ USERNAME=""
 PASSWORD=""
 
 LOG_FILE="/usr/local/hestia/log/system.log"
-key_to_DNSmanager(){
+key_to_DNSmanager(){ 
     # Виконуємо запит для аутентифікації та отримання ключа
-    response=$(curl -s -X POST -d "out=json&func=auth&username=$USERNAME&password=$PASSWORD" "$URL_DNS_MANAGER")
-    key=$(echo "$response" | jq -r '.doc.auth["$"]')
+    response=$(curl -s -X POST -d "out=json&func=auth&lang=ua&username=$USERNAME&password=$PASSWORD" "$URL_DNS_MANAGER")
+    key=$(echo "$response" | jq -r '.doc.auth["$"]') # Номер сесії зберігається протягом години з часу останнього запиту. Якщо протягом години не було виконано жодних запитів, необхідно заново пройти авторизацію.
     if [ -z "$key" ]; then
         echo "Не вдалося отримати ключ. Помилка автентифікації."
         exit 1
@@ -18,14 +18,14 @@ key_to_DNSmanager(){
 
 check_domains_in_DNSmanager(){
     # Перевірка на існування домену
-    check_response=$(curl -s -X POST -d "auth=$key&out=json&func=domain&sok=ok" "$URL_DNS_MANAGER")
+    check_response=$(curl -s -X POST -d "auth=$key&out=json&lang=ua&func=domain&sok=ok" "$URL_DNS_MANAGER")
     domains_in_DNSmanager=$(echo "$check_response" | jq -r '.doc.elem[].name."$"')
     echo -e "$domains_in_DNSmanager"
 }
 
 v_add_dns_domain_to_DNSmanager() {
     # Додаємо тестовий домен через ключ аутентифікації authinfo
-    query=$(curl -s -X POST -d "auth=$key&out=json&func=domain.edit&name=$domen&masterip=$ip_domen&dtype=master&sok=ok" "$URL_DNS_MANAGER")
+    query=$(curl -s -X POST -d "auth=$key&out=json&lang=ua&func=domain.edit&name=$domen&masterip=$ip_domen&dtype=master&sok=ok" "$URL_DNS_MANAGER")
     # query=$(curl -s -X POST -d "auth=$key&out=json&func=domain.edit&name=$domen&masterip=$ip_domen&dtype=slave&sok=ok" "$URL_DNS_MANAGER")
     # query=$(curl -X GET "$URL_DNS_MANAGER?auth=$key&domain=zdimaby.pp.ua&record_type=A&out=json&sok=ok")
     echo "$query"
@@ -70,13 +70,15 @@ v_add_dns_domain() {
     fi
 }
 
-domain_edit(){
-    query=$(curl -s -X POST -d "auth=$key&out=json&func=domain.edit&name=$domen&masterip=$ip_domen&elid=domain.record&sok=ok" "$URL_DNS_MANAGER")
+add_domen_record_to_DNSmanager(){
+    query=$(curl -s -X POST -d "auth=$key&out=json&lang=ua&func=domain.record.edit&plid=$domen&elid=$sub_domen&name=$sub_domen&rtype=$type&ip=$ip_domen&value=$value" "$URL_DNS_MANAGER")
     echo "$query"
 }
 
 domain_record_edit(){
-    query=$(curl -s -X POST -d "auth=$key&out=json&func=domain.record.edit&sok=ok" "$URL_DNS_MANAGER")
+    query=$(curl -s -X POST -d "auth=$key&out=json&lang=ua&func=domain.record" "$URL_DNS_MANAGER")
+    echo -e "$query\n\n\n\n"
+    query=$(curl -s -X POST -d "auth=$key&out=json&lang=ua&func=domain.record.edit&tconvert=punycode&plid=$domen&elid=$domen&sok=ok" "$URL_DNS_MANAGER")
     echo "$query"
 }
 
@@ -107,10 +109,50 @@ v_add_dns_records() {
 
         case "$action" in
         "v-add-dns-record")
-        echo -e "\e[31mdomain_edit\e[0m"
-        domain_edit
+        echo -e "\e[31madd_domen_record_to_DNSmanager\e[0m"
+        add_domen_record_to_DNSmanager
         # echo -e "\e[31mdomain_record_edit\e[0m"
         # domain_record_edit
+            ;;
+        *)
+        echo "$action"
+            ;;
+        esac
+    else
+        echo "Недостатньо даних для розпарсення"
+    fi
+}
+
+domain_delete(){
+    query=$(curl -s -X POST -d "auth=$key&out=json&lang=ua&func=domain.delete&elid=$domen&sok=ok" "$URL_DNS_MANAGER")
+    echo "$query"
+}
+
+v_delete_dns_domain() {
+    # Замінюємо одинарні лапки на порожні рядки за допомогою sed
+    line_without_quotes=$(echo "$line" | sed "s/'//g")
+
+    # Розбиваємо рядок на поля, використовуючи пробіли як роздільник
+    IFS=' ' read -r -a fields <<<"$line_without_quotes"
+
+    # Перевіряємо, чи в рядку є достатньо полів
+    if [ "${#fields[@]}" -ge 4 ]; then
+        timestamp="${fields[0]} ${fields[1]}"
+        action="${fields[2]}"
+        domen="${fields[4]}"
+        ip_domen="${fields[5]}"
+        parameters="${fields[@]:3}" # Всі інші поля як параметри
+
+        echo "Timestamp: $timestamp"
+        echo "Action: $action"
+        echo "Domen: $domen"
+        echo "IP: $ip_domen"
+        echo "Parameters: $parameters"
+
+        case "$action" in
+        "v-delete-dns-domain")
+            echo -e "\e[31mv-delete-dns-domain\e[0m"
+            domain_delete
             ;;
         *)
         echo "$action"
@@ -130,6 +172,10 @@ tail -n0 -f "$LOG_FILE" | grep --line-buffered -E "v-(delete|add|change)-dns-" |
     *v-add-dns-record*)
         echo -e "Викликано /usr/local/hestia/bin/v-add-dns-record\n$line"
         v_add_dns_records
+        ;;
+    *v-delete-dns-domain*)
+        echo -e "Викликано /usr/local/hestia/bin/v-delete-dns-domain\n$line"
+        v_delete_dns_domain
         ;;
     *)
         echo -e "Не можу визначити викликаний метод:\n$line"
