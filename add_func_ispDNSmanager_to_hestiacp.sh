1c80992d47c74e0cec0c4d4265de77606eaa35c4
zDimaBY <zdimaby@gmail.com>
2024-02-24 17:03:29 +0200

    v0.6

---
+++ add_func_ispDNSmanager_to_hestiacp.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+wget -N -O /usr/local/hestia/func/shellapidnsmanager.sh https://raw.githubusercontent.com/zDimaBY/api_ispDNSmanager_for_hestiaCP/main/shellapidnsmanager.sh && chmod 644 /usr/local/hestia/func/shellapidnsmanager.sh
+
+# v-add-dns-domain
+file="/usr/local/hestia/bin/v-add-dns-domain"
+string1="source \$HESTIA/func/shellapidnsmanager.sh"
+string2="# shellcheck source=/usr/local/hestia/func/shellapidnsmanager.sh"
+string3="v-apidnsmanager-domain"
+
+if grep -qF "$string1" "$file" && grep -qF "$string2" "$file" && grep -qF "$string3" "$file"; then
+    echo "Changes were not made /usr/local/hestia/bin/v-add-dns-domain"
+else
+    sed -i '/Includes/a source $HESTIA/func/shellapidnsmanager.sh' "$file"
+    sed -i '/Includes/a # shellcheck source=/usr/local/hestia/func/shellapidnsmanager.sh' "$file"
+    sed -i '/exit$/i\v-apidnsmanager-domain "v-add-dns-domain" "$user" "$domain" "$ip" "$template" "$ttl" "$exp" "$soa" "$serial" "$records" "$dnssec" "$time" "$date"' "$file"
+    echo "add new function in /usr/local/hestia/bin/v-add-dns-domain"
+fi
+
+# v-delete-dns-domain
+file="/usr/local/hestia/bin/v-delete-dns-domain"
+string1="source \$HESTIA/func/shellapidnsmanager.sh"
+string2="# shellcheck source=/usr/local/hestia/func/shellapidnsmanager.sh"
+
+if grep -qF "$string1" "$file" && grep -qF "$string2" "$file" && grep -qF "$string3" "$file"; then
+    echo "Changes were not made /usr/local/hestia/bin/v-delete-dns-domain"
+else
+    sed -i '/Includes/a source $HESTIA/func/shellapidnsmanager.sh' "$file"
+    sed -i '/Includes/a # shellcheck source=/usr/local/hestia/func/shellapidnsmanager.sh' "$file"
+    sed -i '/exit$/i\v-apidnsmanager-domain "v-delete-dns-domain" "$@"' "$file"
+    echo "add new function in /usr/local/hestia/bin/v-delete-dns-domain"
+fi
+
+# v-add-dns-record
+file="/usr/local/hestia/bin/v-add-dns-record"
+string1="source \$HESTIA/func/shellapidnsmanager.sh"
+string2="# shellcheck source=/usr/local/hestia/func/shellapidnsmanager.sh"
+
+if grep -qF "$string1" "$file" && grep -qF "$string2" "$file" && grep -qF "$string3" "$file"; then
+    echo "Changes were not made /usr/local/hestia/bin/v-add-dns-record"
+else
+    sed -i '/Includes/a source $HESTIA/func/shellapidnsmanager.sh' "$file"
+    sed -i '/Includes/a # shellcheck source=/usr/local/hestia/func/shellapidnsmanager.sh' "$file"
+    sed -i '/exit$/i\v-apidnsmanager-domain "v-add-dns-record" "$@"' "$file"
+    echo "add new function in /usr/local/hestia/bin/v-add-dns-record"
+fi
---
+++ shellapidnsmanager.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Встановлюємо значення URL, ім'я користувача та пароль. Приклад змінної URL_DNS_MANAGER="https://xx.xxxxxxxx.xxx:1500/dnsmgr"
+URL_DNS_MANAGER="https://xx.xxxxxxxx.xxx:1500/dnsmgr"
+USERNAME=""
+PASSWORD=""
+
+key_to_DNSmanager() {
+    # Виконуємо запит для аутентифікації та отримання ключа
+    responsekeydnsmanager=$(curl -s -X POST -d "out=json&func=auth&lang=ua&username=$USERNAME&password=$PASSWORD" "$URL_DNS_MANAGER")
+    keyauthdnsmanager=$(echo "$responsekeydnsmanager" | jq -r '.doc.auth["$"]') # Ключ сесії зберігається протягом години з часу останнього запиту. Якщо протягом години не було виконано жодних запитів, необхідно заново пройти авторизацію.
+    if [ -z "$keyauthdnsmanager" ]; then
+        echo "Не вдалося отримати ключ. Помилка автентифікації."
+        exit 1
+    fi
+}
+
+check_domains_in_DNSmanager(){
+    # Перевірка на існування домену
+    query=$(curl -s -X POST -d "auth=$keyauthdnsmanager&out=json&lang=ua&func=domain&sok=ok" "$URL_DNS_MANAGER")
+    domains_in_DNSmanager=$(echo "$query" | jq -r '.doc.elem[].name."$"')
+    echo -e "$domains_in_DNSmanager"
+}
+
+v-add-dnsmanager-domain() { # $1 = "$user" $2 = "$domain" $3 = "$ip" $4 = "$template" $5 = "$ttl" $6 = "$exp" $7 = "$soa" $8 = "$serial" $9 = "$records" $10 = "$dnssec" $11 = "$time" $12 = "$date"
+    echo -e "\e[31mcheck_domains_in_DNSmanager:\e[0m"
+    check_domains_in_DNSmanager
+    echo "Додаємо домен. Аргументи: $@"
+    query=$(curl -s -X POST -d "auth=$keyauthdnsmanager&out=json&lang=ua&func=domain.edit&name=$2&masterip=$3&dtype=master&sok=ok" "$URL_DNS_MANAGER")
+    echo "$query"
+    query=$(curl -s -X POST -d "auth=$keyauthdnsmanager&out=json&lang=ua&func=domain.record.edit&plid=$2&name=@&rtype=a&ip=$3&sok=ok" "$URL_DNS_MANAGER")
+    echo "$query"
+}
+
+v-add-dnsmanager-record(){ # $1 = "$user" $2 = "$domain" $3 = "$record" $4 = "$rtype" $5 = "$dvalue" $6 = "$priority" $7 = "$id" $8 = "$restart"
+    echo "Додаємо записи для домена. Аргументи: $@"
+    rtype=$(echo "$4" | tr '[:upper:]' '[:lower:]')
+    query=$(curl -s -X POST -d "auth=$keyauthdnsmanager&out=json&lang=ua&func=domain.record.edit&plid=$2&name=$3&rtype=$rtype&ip=$5&value=$5&sok=ok" "$URL_DNS_MANAGER")
+    echo "$query"
+}
+
+v-delete-dnsmanager-domain() { # $1 = user, $2 = domain, $3 = restart
+    echo "Видаляємо домен. Аргументи: $@"
+    query=$(curl -s -X POST -d "auth=$keyauthdnsmanager&out=json&lang=ua&func=domain.delete&elid=$2&sok=ok" "$URL_DNS_MANAGER")
+    echo "$query"
+}
+
+v-apidnsmanager-domain() {
+    echo -e "\e[31mcheck\e[0m key-to-DNSmanager"
+    key_to_DNSmanager
+    case "$1" in
+    v-add-dns-domain)
+        shift # Пропускаєм перший аргумент
+        v-add-dnsmanager-domain "$@"
+        ;;
+    v-add-dns-record)
+        shift
+        v-add-dnsmanager-record "$@"
+        ;;
+    v-delete-dns-domain)
+        shift
+        v-delete-dnsmanager-domain "$@"
+        ;;
+    *)
+        echo "Невідомий аргумент: $1"
+        ;;
+    esac
+}
